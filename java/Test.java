e6f6345fadeab231d0c9a69c3208b2c3cd611fff
Deepu joseph <128672682+deepuezhap@users.noreply.github.com>
2024-07-19 08:44:56 +0530

    Add files via upload

--- java/Test.java
+++ java/Test.java
@@ -1,6 +1,65 @@
+import java.io.*;
 public class Test {
+
+
+    static void find(String path){
+        File f = new File(path);
+        File[] files = f.listFiles();
+        for(File fu : files){
+            
+            if(fu.isFile()){
+                System.out.println(fu.getName());
+            }
+            else if(fu.isDirectory()){
+                find(fu.getAbsolutePath());
+            }
+            else{
+                return;
+            }
+        }
+        }
+
+
+        static void search(String path,String name){
+            File f = new File(path);
+            File[] files = f.listFiles();
+            for(File fu : files){
+                if(fu == null){
+                    return;
+                }
+                else if(fu.isFile() && fu.getName().equals(name)){
+                    System.out.println(fu.getName());
+                    System.out.println(fu.getAbsolutePath());
+
+                }
+                else if(fu.isDirectory()){
+                    search(fu.getAbsolutePath(),name);
+                }
+                
+            }
+            }
+
+
+
+
+
+
     public static void main(String[] args) {
-        float f = 1.6f;
-        System.out.println(f);
-    }
+        
+    
+        
+
+        find("Test");
+        System.out.println("-------------");
+        search("Test","inneras.txt");
+            
+
+
+        } 
+
+
+
+
+
+    
 }
